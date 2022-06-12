03c012f38743416501c33416b166ce93fe24da08
kahil kubilay <kubilay.kahilogullari@huawei.com>
2022-06-13 00:05:56 +0300

    added Svelte:Head

--- src/App.svelte
+++ src/App.svelte
@@ -6,10 +6,33 @@
   let pages = ["about", "game"];
   let activePage = "about";
 
+  const description = "Memory game development using Svelte";
+  let image = "images/svelte-pokemons.png";
+  let imageText = "Svelte logo";
+  const title = "Svelte Memory Game";
+
   const switchPage = (event) => {
     activePage = event.detail;
   };
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+
+  <!-- twitter cards -->
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@kahilkubilay" />
+  <meta name="twitter:creator" content="@kahilkubilay" />
+  <meta
+    property="og:url"
+    content="https://github.com/kahilkubilay/remember-em-all"
+  />
+  <meta property="og:title" content={title} />
+  <meta property="og:description" content={description} />
+  <meta property="og:image" content={image} />
+  <meta name="twitter:image:alt" content={imageText} />
+</svelte:head>
 
 <main>
   <Pages {pages} {activePage} on:switchPage={switchPage} />
